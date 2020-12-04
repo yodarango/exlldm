eb735d4b122bcc9130ae5a529561124384cd3d39
Daniel Rangel <danranngel@gmail.com>
2020-12-04 16:15:19 -0500

    third commint

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ app.listen(process.env.PORT || 3000, ()=>
     console.log('listening')
 });
 
-app.get('/', (req, resp)=> 
+app.get('', (req, resp)=> 
 {
-resp.render('index')
+    
+let thoughtOfTheDay = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'+ 
+' standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' + 
+' It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was ' + 
+'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing'
+' software like Aldus PageMaker including versions of Lorem Ipsum'
+let title = 'For Today'
+resp.render('index',
+{
+    title,
+    thoughtOfTheDay
+})
 });
 
 app.get('/testimonios', (req, resp)=> 
