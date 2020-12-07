f5b3e131497225d79beb8c6f5df6f618eb237652
Daniel Rangel <danranngel@gmail.com>
2020-12-07 18:56:21 -0500

    fourth commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs = require('hbs');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+const templatesPath = path.join(__dirname + '/templates');
+
+hbs.registerPartials(templatesPath);
 
 app.listen(process.env.PORT || 3000, ()=> 
 {
@@ -11,17 +16,29 @@ app.listen(process.env.PORT || 3000, ()=>
 
 app.get('', (req, resp)=> 
 {
-    
+    //Thought of the day
 let thoughtOfTheDay = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'+ 
 ' standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' + 
 ' It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was ' + 
 'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing'
 ' software like Aldus PageMaker including versions of Lorem Ipsum'
 let title = 'For Today'
+
+//news section
+
+let newsTitle = 'Lo Encierran por pedofilo y degenarado'
+let newsArticle = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'+ 
+' standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' + 
+' It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was ' + 
+'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing'
+' software like Aldus PageMaker including versions of Lorem Ipsum'
+
 resp.render('index',
 {
     title,
-    thoughtOfTheDay
+    thoughtOfTheDay,
+    newsTitle,
+    newsArticle
 })
 });
 
