526867b81299f327e2a10f2997ca624792bafb06
daniel rangel <dr@daniels-MacBook-Pro.local>
2020-12-08 07:05:44 -0500

    finish all the static pages

--- index.js
+++ index.js
@@ -44,10 +44,21 @@ resp.render('index',
 
 app.get('/testimonios', (req, resp)=> 
 {
-resp.render('testimonios')
+resp.render('testimonios', 
+{
+    title: 'Testimonios Reales de Personas Reales'
+})
 });
 
 app.get('/my-testimonio', (req, resp)=> 
 {
-resp.render('my-testimonio')
+resp.render('my-testimonio',
+{
+    title: 'Yo soy ',
+    testimonyTtile: 'My Historia',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'+ 
+    ' standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' + 
+    ' It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was ' + 
+    'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing'
+})
 }); 
